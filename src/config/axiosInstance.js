c72a03e26f19694f2e5dcbee3dbd299274bed079
Jimmy <jimmy.martel@epitech.eu>
2023-04-12 22:18:06 +0200

    update prod url

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -5,7 +5,7 @@ import axios from "axios"
  */
 const axiosInstance = axios.create({
     // baseURL: process.env.NODE_ENV === "development" ? `http://${process.env.API_HOST}/api` : "https://production.fr",
-    baseURL: process.env.NODE_ENV === "development" ? `/api` : "https://vps-84d4448b.vps.ovh.net/api",
+    baseURL: process.env.NODE_ENV === "development" ? `/api` : "https://mtlk.fr/api",
     headers: {
         'Content-Type': 'application/json'
     }
